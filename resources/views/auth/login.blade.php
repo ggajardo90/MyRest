775c681bf9249f1a62d16a58d31987a650e7e80e
ggajardo90 <ggajardo90@gmail.com>
2022-06-01 00:03:35 -0400

    se modifica vista de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,28 +12,42 @@
 
 <body>
 
+    <img src="{{ asset('img/MyRest (3).png') }}" alt="logo aplicacion" height="150" width="150" >
+    <div class="container mx-auto h-full flex flex-1 justify-center items-center">
+        <div class="w-full max-w-lg">
+            <div class="leading-loose">
 
-    <form action="{{ route('login') }}" method="POST">
-        @csrf
-        username/email
-        <input type="text" name="email" id="" placeholder="email">
-        @error('email')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
+                <form action="{{ route('login') }}" method="POST" class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
+                    @csrf
+                    <h1 class="text-5xl text-gray-800 font-medium flex flex-1 justify-center item-center ">Iniciar Sesion</h1>
+                    <br>
 
-        password
-        <input type="password" name="password" id="" placeholder="password">
-        @error('password')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-        <input type="submit" value="Login">
+                    <div class="mt-2">
+                    <label class="block text-sm text-gray-600" for="cus_email">Usuario</label>
+                    <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="text" name="email" id="" placeholder="email">
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
 
+                    <label class="block text-sm text-gray-600 pt-10" for="cus_email">Contraseña</label>
+                    <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded"  type="password" name="password" id="" placeholder="password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                    <div class="mt-4  text-center pt-10 ">
 
-    </form>
+                    <input class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded " type="submit" value="Login">
+                    <br>
+                        <a class="inline-block right-0 align-baseline font-bold text-sm text-100 hover:text-blue-800"
+                            href="login.html">
+                            Olvidaste tu contraseña?
+                </div>
+
+                </form>
 
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,69 +9,74 @@
     <title>Register</title>
 
 </head>
-<img  src="{{asset('img/MyRest (3).png')}}" alt="logo aplicacion" height="150" width="150">
-<div class="container mx-auto h-full flex flex-1 justify-center items-center">
-    <div class="w-full max-w-lg">
-        <div class="leading-loose">
-            <form action="{{ route('register') }}" method="POST" class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
 
-                @csrf
-                <h1 class="text-5xl text-gray-800 font-medium flex flex-1 justify-center item-center ">Registro</h1>
-                <br>
+<body>
+    <img src="{{ asset('img/MyRest (3).png') }}" alt="logo aplicacion" height="150" width="150">
+    <div class="container mx-auto h-full flex flex-1 justify-center items-center">
+        <div class="w-full max-w-lg">
+            <div class="leading-loose">
 
-                <div class="">
-                    <label class="block text-sm text-gray-00" for="cus_name">Nombre</label>
-                    <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="text" name="name">
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+                <form action="{{ route('register') }}" method="POST"
+                    class="max-w-xl m-4 p-10 bg-white rounded shadow-xl">
 
-                {{-- <div class="mt-2">
+                    @csrf
+                    <h1 class="text-5xl text-gray-800 font-medium flex flex-1 justify-center item-center ">Registro</h1>
+                    <br>
+
+                    <div class="">
+                        <label class="block text-sm text-gray-00" for="cus_name">Nombre</label>
+                        <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="text" name="name">
+                        @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+
+                    {{-- <div class="mt-2">
                     <label class="block text-sm text-gray-00" for="cus_name">Username</label>
                     <input class="w-full px-5 py-1 text-gray-700 bg-gray-200 rounded" type="text" name="username">
                 </div> --}}
 
-                <div class="mt-2">
-                    <label class="block text-sm text-gray-600" for="cus_email">Email</label>
-                    <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="email" name="email">
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
+                    <div class="mt-2">
+                        <label class="block text-sm text-gray-600" for="cus_email">Email</label>
+                        <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="email" name="email">
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                </div>
+                    </div>
 
-                <div class="mt-2">
-                    <label class="block text-sm text-gray-600" for="cus_email">Contraseña</label>
-                    <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="password" name="password">
-                    @error('password')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
+                    <div class="mt-2">
+                        <label class="block text-sm text-gray-600" for="cus_email">Contraseña</label>
+                        <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="password"
+                            name="password">
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
 
-                <div class="mt-2">
-                    <label class="block text-sm text-gray-600" for="cus_email">Confirme Contraseña</label>
-                    <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="password"
-                        name="password_confirmation">
-                </div>
-                <div class="mt-4  text-center pt-10 ">
-                    <input class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded  " type="submit"
-                        value="Registrarse">
-                    <br>
-                    <a class="inline-block right-0 align-baseline font-bold text-sm text-100 hover:text-blue-800"
-                        href="login.html">
-                        Ya tienes una cuenta ?
-                </div>
-            </form>
+                    <div class="mt-2">
+                        <label class="block text-sm text-gray-600" for="cus_email">Confirme Contraseña</label>
+                        <input class="w-full px-5  py-1 text-gray-700 bg-gray-200 rounded" type="password"
+                            name="password_confirmation">
+                    </div>
+                    <div class="mt-4  text-center pt-10 ">
+                        <input class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded  "
+                            type="submit" value="Registrarse">
+                        <br>
+                        <a class="inline-block right-0 align-baseline font-bold text-sm text-100 hover:text-blue-800"
+                            href="login.html">
+                            Ya tienes una cuenta ?
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 </body>
 
