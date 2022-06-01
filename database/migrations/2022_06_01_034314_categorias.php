51710e7ae13353f3091140e9aeac4b4b627d5abb
PRLuis <104658208+PRLuis@users.noreply.github.com>
2022-06-01 00:11:31 -0400

    Clase categoria
    migrate

---
+++ database/migrations/2022_06_01_034314_categorias.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->bigIncrements('id_categoria');
+            $table->string('nombre_categoria');
+            $table->string('descripcion_categoria');
+            $table->string('imagen_categoria');
+            $table->boolean('activo')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
