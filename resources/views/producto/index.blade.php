0792b3ec08e0567beef25fdc1265269fba8fbc6e
lush0n3 <104658208+PRLuis@users.noreply.github.com>
2022-06-30 17:58:04 -0400

    Fix vista producto
    Margenes e imagen

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -43,54 +43,55 @@
 
 
                             <main>
-                                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                                <div class="container">
+                                    <div class="row">
+                                        @foreach ($productos as $producto)
+                                            <div class="col" style="max-width:33%">
+                                                <div class="card shadow-sm">
 
-                                    @foreach ($productos as $producto)
-                                        <div class="col" style='max-width:20%'>
-                                            <div class="card shadow-sm">
+                                                    <?php
+                                                    $imagen = $producto->imagen;
+                                                    if (!file_exists($imagen)) {
+                                                        $imagen = 'img/noimage.png';
+                                                    }
+                                                    ?>
+                                                    <img src="<?php echo $imagen; ?>" class="card-img-top img-fluid">
 
-                                                <?php
-                                                $imagen = $producto->imagen;
-                                                if (!file_exists($imagen)) {
-                                                    $imagen = 'img/nofoto.jpeg';
-                                                }
-                                                ?>
-                                                <img src="<?php echo $imagen; ?>">
+                                                    <div class="card-body">
+                                                        <!--<h5 class="card-title">{{ $producto->id }}</h5>-->
+                                                        <h5 class="card-title">{{ $producto->nombre }}</h5>
+                                                        <p class="card-text">{{ $producto->categoria->nombre }}</p>
+                                                        <p class="card-text">{{ $producto->descripcion }}</p>
+                                                        <h4 class="card-title">${{ $producto->precio }}</h4>
 
-                                                <div class="card-body">
-                                                    <!--<h5 class="card-title">{{ $producto->id }}</h5>-->
-                                                    <h5 class="card-title">{{ $producto->nombre }}</h5>
-                                                    <p class="card-text">{{ $producto->categoria->nombre }}</p>
-                                                    <p class="card-text">{{ $producto->descripcion }}</p>
-                                                    <h4 class="card-title">${{ $producto->precio }}</h4>
+                                                        <div class="d-flex justify-content-between align-items-center"
+                                                            action="{ { route('productos.destroy',$producto->id) }}"
+                                                            method="POST">
+                                                            <div class="btn-group btn-group-sm">
+                                                                <a class="btn btn-sm btn-primary"
+                                                                    href="{{ route('productos.show', $producto->id) }}"><i
+                                                                        class="fa fa-fw fa-eye"></i></a>
 
-                                                    <div class="d-flex justify-content-between align-items-center"
-                                                        action="{ { route('productos.destroy',$producto->id) }}"
-                                                        method="POST">
-                                                        <div class="btn-group">
-                                                            <a class="btn btn-sm btn-primary"
-                                                                href="{{ route('productos.show', $producto->id) }}"><i
-                                                                    class="fa fa-fw fa-eye"></i></a>
+                                                                @can('producto.edit')
+                                                                    <a class="btn btn-sm btn-success"
+                                                                        href="{{ route('productos.edit', $producto->id) }}"><i
+                                                                            class="fa fa-fw fa-edit"></i></a>
+                                                                @endcan
 
-                                                            @can('producto.edit')
-                                                                <a class="btn btn-sm btn-success"
-                                                                    href="{{ route('productos.edit', $producto->id) }}"><i
-                                                                        class="fa fa-fw fa-edit"></i></a>
-                                                            @endcan
-
-                                                            @method('DELETE')
-                                                            @can('producto.destroy')
-                                                                <button type="submit" class="btn btn-danger btn-sm"><i
-                                                                        class="fa fa-fw fa-trash"></i></button>
-                                                            @endcan
-                                                            @csrf
+                                                                @method('DELETE')
+                                                                @can('producto.destroy')
+                                                                    <button type="submit" class="btn btn-sm btn-danger"><i
+                                                                            class="fa fa-fw fa-trash"></i></button>
+                                                                @endcan
+                                                                @csrf
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    @endforeach
+                                        @endforeach
 
+                                    </div>
                                 </div>
                             </main>
                         </div>
